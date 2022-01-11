a85c457f8b177c6bebe77f6050c959a31692b850
henokwel <henokwel@outlook.com>
2022-01-11 21:58:28 +0100

    init filest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import React from 'react'
 import App from './App';
+import { shallow } from 'enzyme';
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />)
+  expect(wrapper).toMatchSnapshot()
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
